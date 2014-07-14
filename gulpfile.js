367dd1c689210785203bfe3a84758200ec2da544
Scott Feeney <scott@oceanbase.org>
2014-07-14 01:21:34 -0400

    Add stuff for an optimized prod build

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var watchify = require('watchify');
+var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify');
+
+var ENTRY_POINT = './js/app.js';
 
 gulp.task('watch', function() {
-  var bundler = watchify('./js/app.js');
+  var bundler = watchify(ENTRY_POINT);
   bundler.on('update', rebundle);
 
   function rebundle() {
@@ -14,6 +18,14 @@ gulp.task('watch', function() {
   }
 
   return rebundle();
+});
+
+gulp.task('build-prod', function() {
+  return (watchify.browserify(ENTRY_POINT)
+    .bundle()
+    .pipe(source('bundle.min.js'))
+    .pipe(streamify(uglify()))
+    .pipe(gulp.dest('.')));
 });
 
 gulp.task('default', ['watch']);
